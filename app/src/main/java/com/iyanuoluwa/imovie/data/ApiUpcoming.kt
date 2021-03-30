b5fd88a3cc7ada31747e1395be2a258a8ee57ed4
ebunoluwa56 <ebunoluwa71@gmail.com>
2021-03-30 12:10:06 +0100

    iMovieApp

--- app/src/main/java/com/iyanuoluwa/imovie/data/ApiUpcoming.kt
+++ app/src/main/java/com/iyanuoluwa/imovie/data/ApiUpcoming.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 // https://api.themoviedb.org/3/movie/upcoming?api_key=68f286331e8795bd4addf043c1e8423d&language=en-US
 
 interface ApiUpcoming {
-    @GET("movie/movie/upcoming?api_key=68f286331e8795bd4addf043c1e8423d&language=en-US")
+    @GET("movie/upcoming?api_key=68f286331e8795bd4addf043c1e8423d&language=en-US")
     suspend fun getUpcomingMovies (): MovieJson
 }
