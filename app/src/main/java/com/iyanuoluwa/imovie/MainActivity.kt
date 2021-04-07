ce63cb883bd3a56fb7cd00eae59a5eddb6a41500
ebunoluwa56 <ebunoluwa71@gmail.com>
2021-04-07 14:15:03 +0100

    iMovieApp

--- app/src/main/java/com/iyanuoluwa/imovie/MainActivity.kt
+++ app/src/main/java/com/iyanuoluwa/imovie/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.iyanuoluwa.imovie
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -15,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     private val topRatedFragment = TopRated()
     private val popularFragment = Popular()
 
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,30 +29,64 @@ class MainActivity : AppCompatActivity() {
         val viewPager = findViewById<ViewPager>(R.id.view_pager)
 
         val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
-        viewPagerAdapter.addFragment(nowPlayingFragment)
-        viewPagerAdapter.addFragment(upcomingFragment)
-        viewPagerAdapter.addFragment(popularFragment)
-        viewPagerAdapter.addFragment(topRatedFragment)
+        viewPagerAdapter.addFragmentToList(nowPlayingFragment)
+        viewPagerAdapter.addFragmentToList(upcomingFragment)
+        viewPagerAdapter.addFragmentToList(popularFragment)
+        viewPagerAdapter.addFragmentToList(topRatedFragment)
+
         viewPager.adapter = viewPagerAdapter
         viewPager.offscreenPageLimit = 3
+        viewPager.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+                return true
+            }
+        })
+
+        bottomNav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nowPlaying -> viewPager.currentItem = 0
+                R.id.upcoming -> viewPager.currentItem = 1
+                R.id.popular -> viewPager.currentItem = 2
+                R.id.topRated -> viewPager.currentItem = 3
+            }
+            false
+        }
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                when(position) {
+                    0 -> bottomNav.menu.findItem(R.id.nowPlaying).isChecked = true
+                    1 -> bottomNav.menu.findItem(R.id.upcoming).isChecked = true
+                    2 -> bottomNav.menu.findItem(R.id.popular).isChecked = true
+                    3 -> bottomNav.menu.findItem(R.id.topRated).isChecked = true
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+
     }
-
-
-
 
 
 
     class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
-        private val fragmentList : MutableList<Fragment> = ArrayList()
+        private val fragmentList : MutableList<Fragment>  = ArrayList()
 
         override fun getCount(): Int {
             return fragmentList.size
         }
 
-        fun addFragment(fragment: Fragment) {
+        fun addFragmentToList(fragment: Fragment) {
             fragmentList.add(fragment)
         }
+
 
         override fun getItem(position: Int): Fragment {
             return fragmentList[position]
