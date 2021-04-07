8d36933f5b1abd58567357c46e0a094c0a2cb711
ebunoluwa56 <ebunoluwa71@gmail.com>
2021-04-07 14:56:21 +0100

    iMovieApp

--- app/src/main/java/com/iyanuoluwa/imovie/adapter/MovieAdapter.kt
+++ app/src/main/java/com/iyanuoluwa/imovie/adapter/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.iyanuoluwa.imovie.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.iyanuoluwa.imovie.DetailsActivity
 import com.iyanuoluwa.imovie.R
 import com.iyanuoluwa.imovie.api.Result
 
@@ -19,7 +21,7 @@ class MovieAdapter(var context: Context,
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.movies_list, parent, false)
-        return ViewHolder(itemView)
+        return ViewHolder(itemView, context)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -35,8 +37,16 @@ class MovieAdapter(var context: Context,
     }
 
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View, context: Context) : RecyclerView.ViewHolder(itemView) {
         var images: ImageView = itemView.findViewById(R.id.movie_image)
         var names: TextView = itemView.findViewById(R.id.movie_name)
+
+        init {
+            itemView.setOnClickListener {
+                val position: Int = adapterPosition
+                val intent = Intent(context, DetailsActivity::class.java)
+                context.startActivity(intent)
+            }
+        }
     }
 }
